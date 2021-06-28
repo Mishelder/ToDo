cd1d600f3ff47e9c0ff4b77662134a1a80c6c2ae
Mishelder <mi3h1@mail.ru>
2021-06-28 03:10:50 +0300

    change processing <form> in the javascript

--- src/com/miaskor/servlets/RegistrationServlet.java
+++ src/com/miaskor/servlets/RegistrationServlet.java
@@ -1,69 +1,40 @@
 package com.miaskor.servlets;
 
-import com.miaskor.dto.RegistrationClientDto;
-import com.miaskor.entity.Client;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.miaskor.exception.ValidationException;
-import com.miaskor.mapper.json.JsonMapper;
+import com.miaskor.mapper.ErrorMessagesToJsonMapper;
+import com.miaskor.mapper.json.JsonToRegistrationClientDtoMapper;
 import com.miaskor.service.RegistrationClientService;
 import com.miaskor.util.ControllersURIKeys;
-import com.miaskor.util.WebFilePath;
+import com.miaskor.util.JsonUtil;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 
 @WebServlet(ControllersURIKeys.REGISTRATION)
 public class RegistrationServlet extends HttpServlet {
 
     private final RegistrationClientService registrationClientService =
             RegistrationClientService.getInstance();
-    private final JsonMapper jsonMapper = new JsonMapper();
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher(WebFilePath.getPath("registration","jsp","jsp"))
-                .forward(req,resp);
-    }
+    private final JsonToRegistrationClientDtoMapper jsonToRegistrationClientDtoMapper =
+            JsonToRegistrationClientDtoMapper.getInstance();
+    private final ErrorMessagesToJsonMapper errorMessagesToJsonMapper =
+            ErrorMessagesToJsonMapper.getInstance();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String clientJson = getBody(req);
-       // var registrationClientDto = jsonMapper.readValue(clientJson);
-       /* System.out.println(registrationClientDto);
+        String body = JsonUtil.parseBody(req);
+        var registrationClientDto = jsonToRegistrationClientDtoMapper.map(body);
         try {
-            var client = registrationClientService.
+            registrationClientService.
                     registerClient(registrationClientDto);
         }catch (ValidationException e){
-            req.setAttribute("error",e.getErrorMessages());
-            resp.setStatus(403);
-        }*/
-    }
-
-    private String getBody(HttpServletRequest req) {
-        StringBuffer stringBuffer = new StringBuffer();
-        try(InputStream inputStream = req.getInputStream()) {
-
-            if (inputStream != null) {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                char[] charBuffer = new char[256];
-                int bytesRead = -1;
-                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-                    stringBuffer.append(charBuffer, 0, bytesRead);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            String errors = errorMessagesToJsonMapper.map(e.getErrorMessages());
+            resp.getWriter().write(errors);
         }
-
-        return stringBuffer.toString();
     }
 }
