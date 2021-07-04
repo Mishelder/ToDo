a83850c62c4a70d6fad1afeef7ef17f848a484a0
Mishelder <mi3h1@mail.ru>
2021-07-04 21:56:50 +0300

    delete date information from servlet

--- src/com/miaskor/servlets/ToDoServlet.java
+++ src/com/miaskor/servlets/ToDoServlet.java
@@ -10,19 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.ZonedDateTime;
 
 @WebServlet(urlPatterns = {ControllersURIKeys.TODO, ControllersURIKeys.TODO + "/start"})
 public class ToDoServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var pointer_time = (ZonedDateTime) req.getSession().getAttribute("pointer_time");
-        var contains = req.getRequestURI().contains("/start");
-        if(contains)
-            pointer_time = (ZonedDateTime) req.getSession().getAttribute("start_time");
-        req.setAttribute("time_cut_off", DateUtil.createTimeCutOffRange(pointer_time));
-        req.setAttribute("time_full_month", DateUtil.createTimeFullMonth(pointer_time));
         req.getRequestDispatcher(WebFilePath.getPath("todo", "jsp", "jsp"))
                 .forward(req, resp);
     }
