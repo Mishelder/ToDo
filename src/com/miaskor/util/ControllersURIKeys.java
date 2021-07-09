8d9a7a24a75ec450bec055f24322eef6f31e0262
Mishelder <mi3h1@mail.ru>
2021-07-09 11:32:04 +0300

    add urls for save, delete, update_done and update servlets

--- src/com/miaskor/util/ControllersURIKeys.java
+++ src/com/miaskor/util/ControllersURIKeys.java
@@ -15,10 +15,15 @@ public class ControllersURIKeys {
     public static final String FLIP_LEFT = "/flip/left";
     public static final String TODO = "/todo";
     public static final String FETCH = "/fetch";
+    public static final String SAVE = "/save";
+    public static final String DELETE = "/delete";
+    public static final String UPDATE_DONE = "/updateDone";
+    public static final String UPDATE = "/update";
 
     public static Set<String> getAllFeasibleURL() {
         return Set.of(LOGIN, REGISTRATION, CSS_LOADER, SAVE_TASK,
-                LOGOUT, FLIP_RIGHT, FLIP_LEFT, TODO, FETCH);
+                LOGOUT, FLIP_RIGHT, FLIP_LEFT, TODO, FETCH,SAVE,
+                DELETE,UPDATE_DONE,UPDATE);
     }
 
     public static Set<String> getAllPublicURL() {
