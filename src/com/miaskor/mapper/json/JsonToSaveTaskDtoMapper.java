09b8e69a4dee1d66e7081e31cc599b1655a2d90b
Mishelder <mi3h1@mail.ru>
2021-07-09 11:36:10 +0300

    add json mapper

---
+++ src/com/miaskor/mapper/json/JsonToSaveTaskDtoMapper.java
@@ -0,0 +1,25 @@
+package com.miaskor.mapper.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miaskor.dto.SaveTaskDto;
+import com.miaskor.mapper.Mapper;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JsonToSaveTaskDtoMapper implements Mapper<String, SaveTaskDto> {
+    private static final JsonToSaveTaskDtoMapper INSTANCE = new JsonToSaveTaskDtoMapper();
+    private final ObjectMapper jsonMapper = new ObjectMapper();
+
+    @SneakyThrows
+    @Override
+    public SaveTaskDto map(String object) {
+        return jsonMapper.readValue(object,SaveTaskDto.class);
+    }
+
+    public static JsonToSaveTaskDtoMapper getInstance(){
+        return INSTANCE;
+    }
+
+}
