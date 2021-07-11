b9a0cbc57a18543796b08b95ef8e548ea0b0078a
Mishelder <mi3h1@mail.ru>
2021-07-11 21:56:37 +0300

    add database for testing

--- src/com/miaskor/dao/ClientDaoImpl.java
+++ src/com/miaskor/dao/ClientDaoImpl.java
@@ -1,6 +1,8 @@
 package com.miaskor.dao;
 
 import com.miaskor.database.ConnectionManager;
+import com.miaskor.database.MainConnectionManager;
+import com.miaskor.database.TestConnectionManager;
 import com.miaskor.entity.Client;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -17,32 +19,38 @@ import java.util.Optional;
 public class ClientDaoImpl implements ClientDao<Integer, Client> {
 
     private static final ClientDaoImpl INSTANCE = new ClientDaoImpl();
+    private static ConnectionManager connectionManager = MainConnectionManager.getInstance();
 
     private static final String CREATE_CLIENT = """ 
-            INSERT INTO to_do_list_repository.public.client
+            INSERT INTO client
             (login, email, password) VALUES (?,?,?)""";
     private static final String READ_CLIENT = """
-            SELECT id,login,email,password FROM to_do_list_repository.public.client""";
+            SELECT id,login,email,password FROM client WHERE id = ?""";
     private static final String UPDATE_CLIENT = """
-            UPDATE to_do_list_repository.public.client
-            SET login = ?,email=?,password=? WHERE id ?""";
+            UPDATE client
+            SET login = ?,email=?,password=? WHERE id = ?""";
     private static final String DELETE_CLIENT_BY_ID = """
-            DELETE FROM to_do_list_repository.public.client WHERE id = ?""";
+            DELETE FROM client WHERE id = ?""";
     private static final String READ_ALL_CLIENT = """
-            SELECT id,login,email,password FROM to_do_list_repository.public.client""";
+            SELECT id,login,email,password FROM client ORDER BY id""";
     private static final String READ_BY_LOGIN = """
-            SELECT id,login,email,password FROM to_do_list_repository.public.client WHERE login = ?""";
+            SELECT id,login,email,password FROM client WHERE login = ?""";
     private static final String READ_BY_EMAIL = """
-            SELECT id,login,email,password FROM to_do_list_repository.public.client WHERE email = ?""";
+            SELECT id,login,email,password FROM client WHERE email = ?""";
 
     public static ClientDaoImpl getInstance() {
+        return INSTANCE;
+    }
+    public static ClientDaoImpl getInstance(boolean isTest) {
+        if(isTest)
+            connectionManager = TestConnectionManager.getInstance();
         return INSTANCE;
     }
 
     @Override
     @SneakyThrows
     public Client create(Client object) {
-        try (var connection = ConnectionManager.getConnection();
+        try (var connection = connectionManager.getConnection();
              var preparedStatement = connection.prepareStatement(CREATE_CLIENT, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setString(1, object.getLogin());
             preparedStatement.setString(2, object.getEmail());
@@ -59,7 +67,7 @@ public class ClientDaoImpl implements ClientDao<Integer, Client> {
     @SneakyThrows
     public Optional<Client> read(Integer index) {
         Client client = null;
-        try (var connection = ConnectionManager.getConnection();
+        try (var connection = connectionManager.getConnection();
              var preparedStatement = connection.prepareStatement(READ_CLIENT)) {
             preparedStatement.setInt(1, index);
             var resultSet = preparedStatement.executeQuery();
@@ -72,24 +80,24 @@ public class ClientDaoImpl implements ClientDao<Integer, Client> {
 
     @Override
     @SneakyThrows
-    public void update(Client object) {
-        try (var connection = ConnectionManager.getConnection();
+    public boolean update(Client object) {
+        try (var connection = connectionManager.getConnection();
              var preparedStatement = connection.prepareStatement(UPDATE_CLIENT)) {
             preparedStatement.setString(1, object.getLogin());
             preparedStatement.setString(2, object.getEmail());
             preparedStatement.setString(3, object.getPassword());
             preparedStatement.setInt(4, object.getId());
-            preparedStatement.execute();
+            return preparedStatement.executeUpdate()>0;
         }
     }
 
     @Override
     @SneakyThrows
     public boolean delete(Integer index) {
-        try (var connection = ConnectionManager.getConnection();
+        try (var connection = connectionManager.getConnection();
              var preparedStatement = connection.prepareStatement(DELETE_CLIENT_BY_ID)) {
             preparedStatement.setInt(1, index);
-            return preparedStatement.execute();
+            return preparedStatement.executeUpdate()>0;
         }
     }
 
@@ -97,7 +105,7 @@ public class ClientDaoImpl implements ClientDao<Integer, Client> {
     @SneakyThrows
     public List<Client> findAll() {
         List<Client> clients = new ArrayList<>();
-        try (var connection = ConnectionManager.getConnection();
+        try (var connection = connectionManager.getConnection();
              var preparedStatement = connection.prepareStatement(READ_ALL_CLIENT)) {
             var resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -111,7 +119,7 @@ public class ClientDaoImpl implements ClientDao<Integer, Client> {
     @SneakyThrows
     public Optional<Client> readByLogin(String login) {
         Client client = null;
-        try (var connection = ConnectionManager.getConnection();
+        try (var connection = connectionManager.getConnection();
              var preparedStatement = connection.prepareStatement(READ_BY_LOGIN)) {
             preparedStatement.setString(1, login);
             var resultSet = preparedStatement.executeQuery();
@@ -126,7 +134,7 @@ public class ClientDaoImpl implements ClientDao<Integer, Client> {
     @SneakyThrows
     public Optional<Client> readByEmail(String email) {
         Client client = null;
-        try (var connection = ConnectionManager.getConnection();
+        try (var connection = connectionManager.getConnection();
              var preparedStatement = connection.prepareStatement(READ_BY_EMAIL)) {
             preparedStatement.setString(1, email);
             var resultSet = preparedStatement.executeQuery();
