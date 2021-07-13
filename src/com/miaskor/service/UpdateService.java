f2417f6598d2719b059ce13cc96bcf83407ea675
Mishelder <mi3h1@mail.ru>
2021-07-14 01:51:33 +0300

    delete update task done cause it is silly and change update method

--- src/com/miaskor/service/UpdateService.java
+++ src/com/miaskor/service/UpdateService.java
@@ -1,10 +1,7 @@
 package com.miaskor.service;
 
 import com.miaskor.dao.TaskDaoImpl;
-import com.miaskor.dto.UpdateTaskDoneDto;
 import com.miaskor.dto.UpdateTaskDto;
-import com.miaskor.entity.Task;
-import com.miaskor.mapper.UpdateTaskDoneDtoToTaskMapper;
 import com.miaskor.mapper.UpdateTaskDtoToTaskMapper;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -13,15 +10,9 @@ import lombok.NoArgsConstructor;
 public class UpdateService {
     private static final UpdateService INSTANCE = new UpdateService();
     private final TaskDaoImpl taskDao = TaskDaoImpl.getInstance(false);
-    private final UpdateTaskDoneDtoToTaskMapper updateTaskDoneDtoToTaskMapper =
-            UpdateTaskDoneDtoToTaskMapper.getInstance();
     private final UpdateTaskDtoToTaskMapper updateTaskDtoToTaskMapper =
             UpdateTaskDtoToTaskMapper.getInstance();
 
-    public void updateDoneTask(UpdateTaskDoneDto updateTaskDoneDto){
-        var mappedTask = updateTaskDoneDtoToTaskMapper.map(updateTaskDoneDto);
-        taskDao.updateTaskDone(mappedTask);
-    }
 
     public void updateTask(UpdateTaskDto updateTaskDto){
         var mappedTask = updateTaskDtoToTaskMapper.map(updateTaskDto);
